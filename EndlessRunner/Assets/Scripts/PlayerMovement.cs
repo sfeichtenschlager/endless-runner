51eb9e15bd8c73246f0429eac9fe504af32d20cb
sfeichtenschlager <feichtenschlager.s@gmx.at>
2023-05-02 09:26:01 +0200

    Changed script structure

--- EndlessRunner/Assets/Scripts/PlayerMovement.cs
+++ EndlessRunner/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public Animator animator;
 
